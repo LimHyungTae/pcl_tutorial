47af8f235888397dd61bde43a059c4c464331cae
Hyungtae Lim <shapelim@kaist.ac.kr>
2021-09-09 09:59:01 +0900

    upload ptr

--- lec2_2_ptr.cpp
+++ lec2_2_ptr.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 template<class T>
 void print_pc(pcl::PointCloud<T> &cloud) {
-    int count = 0;
+    int             count = 0;
     for (const auto &pt: cloud.points) {
         cout << count++ << ": ";
         cout << pt.x << ", " << pt.y << ", " << pt.z << endl;
@@ -16,7 +16,7 @@ void print_pc(pcl::PointCloud<T> &cloud) {
 
 template<class T>
 void print_pc_via_copy(pcl::PointCloud<T> cloud) {
-    int count = 0;
+    int             count = 0;
     for (const auto &pt: cloud.points) {
         cout << count++ << ": ";
         cout << pt.x << ", " << pt.y << ", " << pt.z << endl;
@@ -24,7 +24,7 @@ void print_pc_via_copy(pcl::PointCloud<T> cloud) {
 }
 
 void print_ptr(pcl::PointCloud<pcl::PointXYZ>::Ptr ptr_cloud) {
-    int count = 0;
+    int             count = 0;
     for (const auto &pt: ptr_cloud->points) {
         cout << count++ << ": ";
         cout << pt.x << ", " << pt.y << ", " << pt.z << endl;
@@ -55,7 +55,7 @@ void consptr_test(pcl::PointCloud<pcl::PointXYZ>::ConstPtr ptr_cloud) {
 }
 
 
-int main(int argc, char**argv) {
+int main(int argc, char **argv) {
     pcl::PointCloud<pcl::PointXYZ> cloud2;
     cloud2.push_back(pcl::PointXYZ(1, 2, 3));
     cloud2.push_back(pcl::PointXYZ(4, 5, 6));
@@ -90,7 +90,7 @@ int main(int argc, char**argv) {
     std::cout << ptr_cloud << std::endl;
     // 0x55b586287000
     std::cout << ptr_cloud2 << std::endl;
-    // 0x55b586287000 (93번 줄과 같은 값)
+    // 0x55b586287000 (90번 줄과 같은 값)
 
     // 따라서 print된 것을 확인해보면, ptr_cloud2 뒤에 넣었는데 ptr_cloud의 제일 뒤에도 (-1, -2, -3)이 들어가 있음!
     ptr_cloud2->points.emplace_back(pcl::PointXYZ(-1, -2, -3));
@@ -126,13 +126,13 @@ int main(int argc, char**argv) {
     cout << " ---------------- " << endl;
 
     // 함수 인자로 받을때: 둘다 위에서 출력한 주소와 같은 주소를 출력함
-    // 93~95번째 줄과 같은 주소값을 print함
+    // 90~92번째 줄과 같은 주소값을 print함
     print_address(ptr_cloud);
     print_address2(ptr_cloud);
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr ptr_cloud3(new pcl::PointCloud<pcl::PointXYZ>);
 
-    // 91 번줄과는 다르게 *$Ptr$ = *$Ptr$을 하면 메모리에 있던 정보를 복사해서 넘겨준다는 의미임
+    // 88 번줄과는 다르게 *$Ptr$ = *$Ptr$을 하면 메모리에 있던 정보를 복사해서 넘겨준다는 의미임
     // 따라서, ptr_cloud3와 ptr_cloud는 둘이 독립적임!
     *ptr_cloud3 = *ptr_cloud;
 
@@ -148,7 +148,7 @@ int main(int argc, char**argv) {
     // 5: 0, 0, 1
     // 6: 1, 0, 0
     cout << " ------ $Ptr$ = $Ptr$ ------ " << endl;
-    // ptr_cloud2는 91번째 줄에서 $Ptr$ = $Ptr$로 할당받았는데,
+    // ptr_cloud2는 88번째 줄에서 $Ptr$ = $Ptr$로 할당받았는데,
     // ptr_cloud와 같은 객체를 가리키고 있다보니 add_pc()와 add_pc2() 함수의 영향을 받음
     print_ptr(ptr_cloud2); // changes
     // 0: 1, 2, 3
@@ -160,7 +160,7 @@ int main(int argc, char**argv) {
     // 6: 1, 0, 0 <- add_pc2()함수로 인해 추가됨
 
     cout << " ------ *$Ptr$ = *$Ptr$ ------ " << endl;
-    // ptr_cloud3는 140번째 줄에서 *$Ptr$ = *$Ptr$로 할당받았는데,
+    // ptr_cloud3는 137번째 줄에서 *$Ptr$ = *$Ptr$로 할당받았는데,
     // rvalue Ptr이 가리키던 points를 복사했다보니,
     // ptr_cloud3는 add_pc()와 add_pc2() 함수의 영향 x
     print_ptr(ptr_cloud3); // does not change
